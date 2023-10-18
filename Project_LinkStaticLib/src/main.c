036e889978ff4d6df630d5d3cc9d3a6f3694a0aa
Rein, Wu <gama79530@gmail.com>
2023-10-18 12:53:23 +0800

    Project_LinkStaticLib complete
    1. make a demo program that include static library.

---
+++ Project_LinkStaticLib/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <staticLibInterface.h>
+
+int main(void){
+    printf("Link Static Library Demo.\n");
+    staticLibProg1();
+    staticLibProg2();
+    staticLibProg3();
+
+    return 0;
+}
